e71927da0298d424ee39cc8fb19c0f5d461da10a
Fernanda Núñez <angelestradamx@hotmail.com>
2020-04-04 22:32:28 -0600

    NetworkState

---
+++ NetworkState/app/src/main/java/com/example/networkstate/NetworkConnectionState.kt
@@ -0,0 +1,61 @@
+package com.example.networkstate
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.util.Log
+
+class NetworkConnectionState(val context: Context) {
+
+    fun IsNetworkConnected():Boolean{
+
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M){
+
+            val network = connectivityManager.activeNetwork
+
+            if(network!=null){
+
+                connectivityManager.getNetworkCapabilities(network).run {
+                    return when {
+                        hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                            Log.d("UDELP","TRANSPORT_WIFI")
+                            true
+                        }
+                        hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                            Log.d("UDELP","TRANSPORT_CELLULAR")
+                            true
+                        }
+                        hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                            Log.d("UDELP","TRANSPORT_ETHERNET")
+                            true
+                        }
+                        else -> {
+                            Log.d("UDELP","NADA")
+                            false
+                        }
+                    }
+                }
+
+            }else{
+
+                Log.d("UDELP","NETWORK_NULL")
+                return false
+
+            }
+
+        }else{
+
+            Log.d("UDELP","OLD_VERSION")
+            connectivityManager.activeNetworkInfo.run {
+
+                return isConnected
+
+            }
+
+        }
+
+    }
+
+}
